d9b2881a2971c3db765d2cc331a25e851fe89938
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-09-02 00:46:15 -0400

    fourth commit

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const mongoose = require ("mongoose")
 
+const express = require("express")
+const app = express()
+
 const DATABASE = "mongodb://mymongo:27017/test"
 
 mongoose.connect(DATABASE)
    .then(() => {console.log("DB is running")}) 
    .catch(() => {console.log("DB ERROR")})
+
+
+app.get("/", (req, res) => {
+   res.json({
+      message: "Hello World"
+   })
+})
+
+app.listen(8000, () => {
+   console.log("App is running at port 3000")
+})
+
