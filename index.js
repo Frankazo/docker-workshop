38f263eb360c82df528f7b2b0f87460e33d00f24
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-09-02 00:40:07 -0400

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose")
 
-const DATABASE = "mongodb://localhost:27017/test"
+const DATABASE = "mongodb://mymongo:27017/test"
 
 mongoose.connect(DATABASE)
    .then(() => {console.log("DB is running")}) 
